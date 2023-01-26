778fd5b671b572450b5c951ede33365f740d552b
gustavonardino <fgustavo.n@gmail.com>
2023-01-26 14:44:48 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from '../styles/Home.module.css'
+
+import { S } from '../styles/teste'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <S.Wrapper>
+        <S.Title>
+          Hello World!
+        </S.Title>
+      </S.Wrapper>
+    </>
+  )
+}
---
+++ src/styles/teste.ts
@@ -0,0 +1,31 @@
+
+import styled, { StyledComponent } from 'styled-components'
+
+interface dadosType {
+  estilo1: string,
+  estilo2: string,
+}
+
+//ESTILOS VINDOS DO STATE
+const dados: dadosType = {
+  estilo1: 'center',
+  estilo2: 'yellow',
+}
+
+//DESENHA O LAYOUT BÁSICO E DINAMIZA SÓ O QUE QUISER
+const Title: StyledComponent<"h1", any, {}, never> = styled.h1`
+  font-size: 1.5em;
+  text-align: ${dados.estilo1};
+  color: palevioletred;
+`;
+
+
+const Wrapper: StyledComponent<"section", any, {}, never> = styled.section`
+  padding: 4em;
+  background: ${dados.estilo2};
+`;
+
+export const S = {
+  Title,
+  Wrapper
+}
